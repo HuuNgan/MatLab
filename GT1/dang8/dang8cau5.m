8301138638cb9088fb24bdc434012859b2a5ed65
HuuNgan <ngan.luongbkdev@hcmut.edu.vn>
2019-08-06 07:23:41 +0700

    fix some mistake

--- GT1/dang8/dang8cau5.m
+++ GT1/dang8/dang8cau5.m
@@ -1,7 +1,7 @@
 syms x;
 	y=sqrt(2-x^2);
     y1=diff(y,x)
-	L=2*int(sqrt(1+y1^2),x) %vi x^2=2-y^2,nen 0<=x<sqrt(2)
+	L=int(sqrt(1+y1^2),x) %vi x^2=2-y^2,nen 0<=x<sqrt(2)
     a=limit(L,x,-sqrt(2))
     b=limit(L,x,sqrt(2))
     L=b-a
